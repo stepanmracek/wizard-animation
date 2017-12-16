732a13fb1ad196be457ea06f460bc8c978db5212
Stepan Mracek <stepan.mracek@gmail.com>
2017-12-16 22:27:57 +0100

    basic wizard animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('nextAnimation', [
+      state('beforeNext', style({
+        opacity: 0,
+        transform: 'translateX(100%)'
+      })),
+      state('afterNext', style({
+        transform: 'translateX(0)'
+      })),
+      transition('beforeNext => afterNext', animate('250ms ease-in'))
+    ]),
+    trigger('prevAnimation', [
+      state('beforePrev', style({
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      })),
+      state('afterPrev', style({
+        transform: 'translateX(0)'
+      })),
+      transition('beforePrev => afterPrev', animate('250ms ease-in'))
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  currentComponent: Function = null;
+  componentStack: Function[] = [];
+  animationState: string;
+
+  ngOnInit() {
+    this.push(Step1Component);
+  }
+
+  private push(component: Function) {
+    this.componentStack.push(component);
+    this.currentComponent = component;
+    this.animationState = 'beforeNext';
+    setTimeout(() => { this.animationState = 'afterNext'; });
+    console.log('after push', this.componentStack.map(c => c.name).join(', '));
+  }
+
+  private pop() {
+    if (this.componentStack.length > 1)
+      this.componentStack.pop();
+    this.currentComponent = this.componentStack[this.componentStack.length - 1];
+    this.animationState = 'beforePrev';
+    setTimeout(() => { this.animationState = 'afterPrev'; });
+    console.log('after pop', this.componentStack.map(c => c.name).join(', '));
+  }
+
+  private getRandomComponent() {
+    const options = [Step1Component, Step2Component, Step3Component];
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+  }
+
+  next() {
+    const component = this.getRandomComponent();
+    this.push(this.getRandomComponent());
+  }
+
+  prev() {
+    this.pop();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Step1Component,
+    Step2Component,
+    Step3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  entryComponents: [
+    Step1Component,
+    Step2Component,
+    Step3Component
   ],
   providers: [],
   bootstrap: [AppComponent]
